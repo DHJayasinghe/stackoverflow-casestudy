df1371d49dc2158fab26735e26143ca8375dc02f
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2023-01-16 05:13:55 +0530

    Script changes

--- database/seeding/01-tags-and-posttags-data.sql
+++ database/seeding/01-tags-and-posttags-data.sql
@@ -1,6 +1,37 @@
 USE [StackOverflow2010]
 GO
 
+CREATE  OR ALTER FUNCTION [dbo].[splitstring](
+	@stringToSplit nvarchar(max),
+	@seperator varchar(8000)
+)
+RETURNS
+ @returnList TABLE ([value] nvarchar(max))
+AS
+BEGIN
+	declare @searchTerm varchar(8000)=@seperator
+	declare @searchTermStartPos int
+	declare @searchTermEndPos int
+	declare @searchResult varchar(max)
+
+	WHILE CHARINDEX(@searchTerm, @stringToSplit) > 0
+	BEGIN
+		SET @searchTermStartPos = (SELECT CHARINDEX(@searchTerm,@stringToSplit))
+		SET @searchTermEndPos = @searchTermStartPos + LEN(@searchTerm)
+		SET @searchResult = (SELECT SUBSTRING(@stringToSplit,1,@searchTermStartPos-1))
+		SET @stringToSplit = (SELECT SUBSTRING(@stringToSplit,@searchTermEndPos,LEN(@stringToSplit)))
+
+		INSERT INTO @returnList 
+		SELECT @searchResult
+	END
+
+	INSERT INTO @returnList
+	SELECT @stringToSplit
+
+	RETURN
+END
+GO
+
 DECLARE @postsTags TABLE(id int,val varchar(1000))
 INSERT INTO @postsTags(id,val)
 
@@ -15,3 +46,4 @@ INSERT INTO dbo.PostTags(PostId,TagId)
 SELECT PT.id,T.Id
 FROM @postsTags PT INNER JOIN dbo.Tags T
 ON PT.val=T.TagName
+GO
