c06f673f59604fc9dc42efe6d3b3a39a8470579c
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2023-01-16 07:05:06 +0530

    Question search API with Pagination support

---
+++ stackoverflow-api/stackoverflow-api/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿internal record Question
+{
+    public int Id { get; init; }
+    public string Title { get; init; }
+    public string Description { get; init; }
+    public string Tags { get; init; }
+    public DateTime AskedDateTime { get; init; }
+    public int AskedById { get; init; }
+    public string AskedByDisplayName { get; init; }
+    public int NoOfVotes { get; init; }
+}
+
+internal record PaginationResult<T> where T : class
+{
+    public int TotalRecords { get; init; }
+    public int FilteredRecords => Data.Count;
+    public List<T> Data { get; init; }
+}
