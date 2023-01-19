e301ebebcc3edb2681ab18c9b9f542421d9436a3
Dhanuka Jayasinghe <dhanuka@jrcsoftware.com>
2023-01-19 20:13:41 +0530

    Multi-search param support and Votes count

--- stackoverflow-api/stackoverflow-api/Program.cs
+++ stackoverflow-api/stackoverflow-api/Program.cs
@@ -20,9 +20,18 @@ static async Task<PaginationResult<Question>> GetQuestionsAsync(
     int start = 0
 )
 {
-    string predicate = !string.IsNullOrEmpty(searchTerm) ? $" WHERE TagName = '{searchTerm}'" : "";
+    var searchTerms = searchTerm.Split(",",StringSplitOptions.RemoveEmptyEntries);
+    bool IsMultiSearch() => searchTerms.Length > 1;
+
+    var query_params = new
+    {
+        SearchTerm = searchTerms,
+        Start = start,
+        PageSize = pageSize
+    };
+    string predicate = !string.IsNullOrEmpty(searchTerm) ? (IsMultiSearch() ? " WHERE TagName IN @SearchTerm" : $" WHERE TagName = @SearchTerm") : "";
     var totalCountQuery = @$"SELECT COUNT(1) FROM dbo.vwPostTags with(noexpand) {predicate}";
-    var totalRecords = (await dbQueryRepo.QueryAsync<int>(totalCountQuery)).First();
+    var totalRecords = (await dbQueryRepo.QueryAsync<int>(totalCountQuery, query_params)).First();
     var query = @$"SELECT
             P.Id,
             P.Title,
@@ -31,17 +40,18 @@ static async Task<PaginationResult<Question>> GetQuestionsAsync(
             P.CreationDate AskedDateTime,
             P.OwnerUserId AskedById,
             Own.DisplayName AskedByDisplayName,
-            1 Votes
+            V.Votes NoOfVotes
         FROM dbo.Posts P INNER JOIN(SELECT PostId, TagId, CreationDate
         FROM dbo.vwPostTags with(noexpand) {predicate}
         ORDER BY CreationDate
-        OFFSET({start}) ROWS FETCH NEXT({pageSize}) ROWS ONLY) PT
-        ON P.Id = PT.PostId INNER JOIN dbo.Users Own
+        OFFSET(@Start) ROWS FETCH NEXT(@PageSize) ROWS ONLY) PT
+        ON P.Id = PT.PostId LEFT JOIN (SELECT V.PostId,COUNT(1) Votes FROM dbo.Votes V GROUP BY V.PostId) V 
+        ON P.Id = V.PostId LEFT JOIN dbo.Users Own
         ON P.OwnerUserId = Own.Id";
 
-    var result = await dbQueryRepo.QueryAsync<Question>(query);
+    var result = await dbQueryRepo.QueryAsync<Question>(query, query_params);
     return new PaginationResult<Question>()
-{
+    {
         Data = result,
         TotalRecords = totalRecords
     };
