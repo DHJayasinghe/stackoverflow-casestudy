99746fba04765e53ea5db62b56a5972bf3f45892
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2023-02-23 08:00:20 +0530

    Refactored stored procedure

--- database/schema/04-posts-vote.sql
+++ database/schema/04-posts-vote.sql
@@ -12,28 +12,28 @@ BEGIN
 	BEGIN TRY
 		BEGIN TRANSACTION
 
+		-- We only consider UpVote or DownVote
 		SELECT 
 			@existingVoteId=Id,
 			@existingVoteTypeId=VoteTypeId 
 		FROM dbo.Votes 
 		WHERE PostId=@Id 
 			AND UserId=@UserId 
-			AND VoteTypeId IN (2,3)
+			AND VoteTypeId IN (2,3) 
 
-		IF @existingVoteId = 0 -- Here there was no matching records, so we insert new
+		IF @existingVoteId = 0 
 		BEGIN	
+			-- Here, there was no previous Vote from this user, so we INSERT one
 			INSERT INTO dbo.Votes(PostId,UserId,VoteTypeId,CreationDate)
 			VALUES(@id,@userId,@voteTypeId,GETUTCDATE())
 
-			SET @noOfChanges=@@ROWCOUNT
+			SET @noOfChanges=1
 		END
 
 		IF @existingVoteId != 0 AND @existingVoteTypeId != @VoteTypeId
 		BEGIN
-			-- Here we decided to update any existing Vote row for that specific user, for specific Post, with VoteType 1 or 2 as a fresh entry.
-			-- Cause DELETE existing and INSERT new row each time voting has to regenerate a new ID
-			-- This way we won't ran out of INT for our Primary Key
-
+			-- Here, we found a previous vote from this user, but the Vote has changed. So we UPDATE it
+			-- And of course to make sure we don't ran out of int fast for Ids.
 			UPDATE dbo.Votes SET 
 				VoteTypeId=@VoteTypeId,
 				CreationDate=GETUTCDATE()
@@ -42,12 +42,12 @@ BEGIN
 			SET @noOfChanges=@@ROWCOUNT
 		END
 
-		declare @score int
-
-		SELECT @score=SUM(CASE VoteTypeId WHEN 2 THEN 1 ELSE -1 END) FROM dbo.Votes WHERE PostId=@id AND VoteTypeId IN (2,3)
-
 		IF @noOfChanges > 0 -- Here we found an update/insert, so we update the Posts table score 
 		BEGIN
+			-- Calculate the Score for this Post first
+			declare @score int
+			SELECT @score=SUM(CASE VoteTypeId WHEN 2 THEN 1 ELSE -1 END) FROM dbo.Votes WHERE PostId=@id AND VoteTypeId IN (2,3)
+
 			UPDATE dbo.Posts SET 
 				Score=@score,
 				LastActivityDate=GETUTCDATE()
