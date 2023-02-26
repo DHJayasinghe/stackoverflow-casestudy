603e65c934352567e688d4e20de6f89cc81a55f4
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2023-02-27 00:29:39 +0530

    Integration Testing for Phantom Reads

--- stackoverflow-api/stackoverflow-integration-test/DatabaseConcurrencyTest.cs
+++ stackoverflow-api/stackoverflow-integration-test/DatabaseConcurrencyTest.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using stackoverflow.api;
 using stackoverflow.core;
 using System.Net.Http.Json;
 
@@ -13,7 +12,6 @@ namespace stackoverflow_integration_test;
 public class DatabaseConcurrencyTest
 {
     private WebApplicationFactory<Program> _factory;
-    private HttpClient _httpClient;
 
     public DatabaseConcurrencyTest()
     {
@@ -28,10 +26,9 @@ public class DatabaseConcurrencyTest
                 config.AddConfiguration(integrationConfig);
             });
         });
-        _httpClient = _factory.CreateClient();
     }
 
-    private async Task ResetVoteMadeByUsersAsync(string[] displayNames)
+    private async Task ResetVoteMadeByUsersAsync(int postId, string[] displayNames)
     {
         using var scope = _factory.Services.GetService<IServiceScopeFactory>().CreateScope();
         var queryRepo = scope.ServiceProvider.GetRequiredService<IDbQueryRepository>();
@@ -39,9 +36,20 @@ public class DatabaseConcurrencyTest
                         V.VoteTypeId = 3
                     FROM dbo.Votes V INNER JOIN dbo.Users U
                     ON V.UserId = U.Id AND V.VoteTypeId = 2
-                    WHERE U.[DisplayName] IN @displayNames",
+                    WHERE V.PostId = @postId AND U.[DisplayName] IN @displayNames",
                 commandType: System.Data.CommandType.Text,
-                parameters: new { displayNames = displayNames });
+                parameters: new { postId = postId, displayNames = displayNames });
+    }
+
+    private async Task RemoveVoteMadeByUsersAsync(int postId, string[] displayNames)
+    {
+        using var scope = _factory.Services.GetService<IServiceScopeFactory>().CreateScope();
+        var queryRepo = scope.ServiceProvider.GetRequiredService<IDbQueryRepository>();
+        await queryRepo.ExecuteAsync(@"DELETE V FROM dbo.Votes V INNER JOIN dbo.Users U
+                    ON V.UserId = U.Id AND V.VoteTypeId = 2
+                    WHERE V.PostId = @postId AND U.[DisplayName] IN @displayNames",
+                commandType: System.Data.CommandType.Text,
+                parameters: new { postId = postId, displayNames = displayNames });
     }
 
     private async Task<int> GetActualScoreOfPostAsync(int id)
@@ -53,6 +61,19 @@ public class DatabaseConcurrencyTest
             mapItems: new List<OutputResultTranform> { new OutputResultTranform(typeof(int), TransformCategory.Single, "Score"),
         });
         return (int)result["Score"];
+    }
+
+    private async Task<int> GetVoteCountOnPostByUserAsync(int id, string userDisplayName)
+    {
+        using var scope = _factory.Services.GetService<IServiceScopeFactory>().CreateScope();
+        var queryRepo = scope.ServiceProvider.GetRequiredService<IDbQueryRepository>();
+        var result = await queryRepo.QueryAsync(@$"SELECT COUNT(1) FROM dbo.Votes V INNER JOIN dbo.Users U ON V.UserId=U.Id 
+                WHERE PostId=@id AND VoteTypeId IN (2,3) AND U.DisplayName = @userDisplayName",
+            parameters: new { id, userDisplayName },
+            commandType: System.Data.CommandType.Text,
+            mapItems: new List<OutputResultTranform> { new OutputResultTranform(typeof(int), TransformCategory.Single, "Vote"),
+        });
+        return (int)result["Vote"];
     }
 
     private async Task<int> GetRecordedScoreOfPostAsync(int id)
@@ -67,12 +88,12 @@ public class DatabaseConcurrencyTest
     }
 
     [TestMethod]
-    public async Task Should_HaveCorrectScore_When_MultipleUsersConcurrentyVoteTheSamePost()
+    public async Task Should_HaveCorrectScore_When_MultipleUsersConcurrentyChangeTheirVoteOnSamePost()
     {
         const int postId = 4175774;
         string UpVoteRequestUri = $"/posts/{postId}/vote/2";
         var selectedUsersDisplayNames = new[] { "Jeff Atwood", "Geoff Dalgas" };
-        await ResetVoteMadeByUsersAsync(selectedUsersDisplayNames);
+        await ResetVoteMadeByUsersAsync(postId, selectedUsersDisplayNames);
 
         var upVotingTasks = await DoConcurrentUpVotingRequests(UpVoteRequestUri, selectedUsersDisplayNames);
         var recordedScore = await GetRecordedScoreOfPostAsync(postId);
@@ -80,6 +101,24 @@ public class DatabaseConcurrencyTest
 
         upVotingTasks.Should().AllSatisfy(response => response.Result.IsSuccessStatusCode.Should().BeTrue());
         recordedScore.Should().Be(actualScore);
+    }
+
+
+    [TestMethod]
+    public async Task Should_HaveSingleVotePerUser_When_UserDoConcurrentVotingOnSamePost()
+    {
+        const int postId = 4175774;
+        string UpVoteRequestUri = $"/posts/{postId}/vote/2";
+        var selectedUsersDisplayNames = new[] { "Jarrod Dixon", "Jarrod Dixon" };
+        await RemoveVoteMadeByUsersAsync(postId, selectedUsersDisplayNames);
+
+        var votesCountBefore = await GetVoteCountOnPostByUserAsync(postId, selectedUsersDisplayNames[0]);
+        var upVotingTasks = await DoConcurrentUpVotingRequests(UpVoteRequestUri, selectedUsersDisplayNames);
+        var votesCountAfter = await GetVoteCountOnPostByUserAsync(postId, selectedUsersDisplayNames[0]);
+
+        upVotingTasks.Should().AllSatisfy(response => response.Result.IsSuccessStatusCode.Should().BeTrue());
+        votesCountBefore.Should().Be(0);
+        votesCountAfter.Should().Be(1);
     }
 
     private async Task<IEnumerable<Task<HttpResponseMessage>>> DoConcurrentUpVotingRequests(string UpVoteRequestUri, string[] usersDisplayNames)
